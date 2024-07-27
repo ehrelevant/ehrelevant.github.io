f09583752968ef9670b493c939059ac7d844aca9
Ehren Castillo <ehrencastillo@gmail.com>
2024-07-28 02:50:30 +0800

    feat: add section headers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+	<h1>Ehren Castillo</h1>
+	<h2>Computer Science Student | Web Developer | Syn-tax Evader</h2>
+	<div></div>
+</section>
+
+<!--Languages, Frameworks, Tools-->
+<section>
+	<h1>Skills</h1>
+	<div></div>
+</section>
+
+<!--Portfolio-->
+<section>
+	<h1>Portfolio</h1>
+	<div></div>
+</section>
+
+<!--Contact Me-->
+<section>
+	<h1>Contact Me!</h1>
+	<div></div>
+</section>
