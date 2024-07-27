7788ae12f4a2a3822e5de66bc31eaae71db9691e
Ehren Castillo <ehrencastillo@gmail.com>
2024-07-28 02:51:37 +0800

    feat: add short bio text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <section>
 	<h1>Ehren Castillo</h1>
 	<h2>Computer Science Student | Web Developer | Syn-tax Evader</h2>
-	<div></div>
+	<div>
+		<p>
+			Hello! I'm Ehren Castillo! A BS Computer Science student at the University of the Philippines
+			- Diliman. I have a deep passion for programming and a keen interest in both web and software
+			development. My academic journey and personal projects have equipped me with a solid
+			foundation in coding, problem-solving, and creating innovative solutions.
+		</p>
+		<p>
+			In my free time, I enjoy exploring new technologies, working on web development projects, and
+			honing my skills in software development. I'm always eager to learn and take on new challenges
+			that push the boundaries of my knowledge and abilities.
+		</p>
+	</div>
 </section>
 
 <!--Languages, Frameworks, Tools-->
