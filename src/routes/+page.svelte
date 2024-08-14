0782053ca0c828e35930008d5a93f6875b969b82
Ehren Castillo <ehrencastillo@gmail.com>
2024-08-15 02:39:58 +0800

    feat: split first section into two

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<section class="h-screen w-full flex flex-col gap-4 justify-center items-center font-light">
+	<h1 class="text-7xl">Ehren Castillo</h1>
+	<h2 class="text-2xl">
+		Computer Science Student | Web Developer | Tech Enthusiast | Syn-tax Evader
+	</h2>
+</section>
+
+<!--Short Description/Spiel-->
 <section>
-	<h1>Ehren Castillo</h1>
-	<h2>Computer Science Student | Web Developer | Syn-tax Evader</h2>
 	<div>
 		<p>
 			Hello! I'm Ehren Castillo! A BS Computer Science student at the University of the Philippines
