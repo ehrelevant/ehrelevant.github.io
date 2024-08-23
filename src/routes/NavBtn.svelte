a7995bfad8b69de95b7c22013fdb31940949e003
Ehren Castillo <ehrencastillo@gmail.com>
2024-08-23 20:41:21 +0800

    fix: attempt to fix color issue for section highlight

--- src/routes/NavBtn.svelte
+++ src/routes/NavBtn.svelte
@@ -2,7 +2,7 @@
 	export let href: string;
 	export let isCurrent = false;
 
-	$: colors = isCurrent ? 'bg-dark-gray text-white' : 'hover:bg-gray-100';
+	$: colors = isCurrent ? 'bg-slate-800 text-white' : 'hover:bg-slate-100';
 </script>
 
 <a {href} class="block px-8 py-2 rounded-xl text-lg font-semibold transition-colors {colors}"
