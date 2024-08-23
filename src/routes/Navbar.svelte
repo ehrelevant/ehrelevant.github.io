5685c4020f4146d22426b72224a278255ff78879
Ehren Castillo <ehrencastillo@gmail.com>
2024-08-23 19:29:14 +0800

    feat(navbar): add transition effect on hover

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,17 +1,23 @@
 <nav class="fixed top-0 w-full bg-white shadow-lg shadow-white">
 	<ul class="flex justify-center gap-2 m-2">
 		<li>
-			<a href="#hero" class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100"
+			<a
+				href="#hero"
+				class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100 transition-colors"
 				>Ehren</a
 			>
 		</li>
 		<li>
-			<a href="#about" class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100"
+			<a
+				href="#about"
+				class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100 transition-colors"
 				>About</a
 			>
 		</li>
 		<li>
-			<a href="#skills" class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100"
+			<a
+				href="#skills"
+				class="block px-8 py-2 rounded-xl text-lg font-semibold hover:bg-gray-100 transition-colors"
 				>Skills</a
 			>
 		</li>
