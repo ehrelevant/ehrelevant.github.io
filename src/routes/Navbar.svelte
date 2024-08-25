e327160102caf0f6c202ac51fff8816c4ec7cc81
Ehren Castillo <ehrencastillo@gmail.com>
2024-08-25 18:47:53 +0800

    feat(navbar): add Projects section to navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -8,8 +8,10 @@
 	function changeSection() {
 		const aboutSectionTop = document.getElementById('about')?.getBoundingClientRect().top;
 		const skillsSectionTop = document.getElementById('skills')?.getBoundingClientRect().top;
+		const projectsSectionTop = document.getElementById('projects')?.getBoundingClientRect().top;
 
-		if (skillsSectionTop !== undefined && skillsSectionTop <= topOffset) section = 2;
+		if (skillsSectionTop !== undefined && skillsSectionTop <= topOffset) section = 3;
+		else if (projectsSectionTop !== undefined && projectsSectionTop <= topOffset) section = 2;
 		else if (aboutSectionTop !== undefined && aboutSectionTop <= topOffset) section = 1;
 		else section = 0;
 	}
@@ -23,13 +25,16 @@
 <nav class="fixed top-0 w-full bg-white shadow-lg shadow-white">
 	<ul class="flex justify-center gap-2 m-2">
 		<li>
-			<NavBtn href="#hero" isCurrent={section === 0}>Ehren</NavBtn>
+			<NavBtn href="#hero" isCurrent={section === 0}>Intro</NavBtn>
 		</li>
 		<li>
 			<NavBtn href="#about" isCurrent={section === 1}>About</NavBtn>
 		</li>
 		<li>
-			<NavBtn href="#skills" isCurrent={section === 2}>Skills</NavBtn>
+			<NavBtn href="#projects" isCurrent={section === 2}>Projects</NavBtn>
+		</li>
+		<li>
+			<NavBtn href="#skills" isCurrent={section === 3}>Skills</NavBtn>
 		</li>
 	</ul>
 </nav>
