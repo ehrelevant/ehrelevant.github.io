becbab70bb7be3d8565b146fa86e32ada16c5551
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-01 18:39:37 +0800

    feat(navbar): add mobile-friendly navbar

---
+++ src/routes/MobileNavBtn.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let href: string;
+	export let isCurrent = false;
+
+	$: colors = isCurrent ? 'bg-dark-gray text-white' : 'hover:bg-slate-100';
+</script>
+
+<a
+	{href}
+	class="block w-full py-4 text-center rounded-xl text-2xl font-semibold hover:scale-105 transition-all {colors}"
+	><slot /></a
+>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { Bars3, XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { fly } from 'svelte/transition';
 	import NavBtn from './NavBtn.svelte';
+	import MobileNavBtn from './MobileNavBtn.svelte';
+	import { quadInOut } from 'svelte/easing';
 
 	let section = -1;
 	const topOffset = 50;
@@ -20,9 +25,19 @@
 		document.addEventListener('scroll', changeSection);
 		changeSection();
 	});
+
+	let isMenuOpen = false;
+
+	function openMenu() {
+		isMenuOpen = true;
+	}
+
+	function closeMenu() {
+		isMenuOpen = false;
+	}
 </script>
 
-<nav class="fixed top-0 z-50 w-full bg-white shadow-lg shadow-white">
+<nav class="hidden sm:block fixed top-0 z-50 w-full bg-white shadow-lg shadow-white">
 	<ul class="flex justify-center gap-2 m-2">
 		<li>
 			<NavBtn href="#intro" isCurrent={section === 0}>Intro</NavBtn>
@@ -37,4 +52,37 @@
 			<NavBtn href="#skills" isCurrent={section === 3}>Skills</NavBtn>
 		</li>
 	</ul>
+</nav>
+
+<nav class="block sm:hidden fixed w-full h-screen z-50">
+	{#if isMenuOpen}
+		<div
+			class="w-full h-full bg-white flex justify-center items-start"
+			transition:fly={{ duration: 100, x: 500, easing: quadInOut }}
+		>
+			<button class="absolute top-3 right-3" on:click={closeMenu}
+				><Icon src={XMark} class="size-8" /></button
+			>
+			<ul class="flex flex-col w-full justify-center gap-2 m-4 mt-16">
+				<li transition:fly={{ delay: 50, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="#intro" isCurrent={section === 0}>Intro</MobileNavBtn>
+				</li>
+				<li transition:fly={{ delay: 75, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="#about" isCurrent={section === 1}>About</MobileNavBtn>
+				</li>
+				<li transition:fly={{ delay: 100, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="#projects" isCurrent={section === 2}>Projects</MobileNavBtn>
+				</li>
+				<li transition:fly={{ delay: 125, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="#skills" isCurrent={section === 3}>Skills</MobileNavBtn>
+				</li>
+			</ul>
+		</div>
+	{:else}
+		<button
+			class="bg-dark-gray rounded-full p-4 absolute top-2 right-2 hover:scale-105 hover:bg-gray-700 transition-all"
+			transition:fly={{ duration: 100, x: 500, easing: quadInOut }}
+			on:click={openMenu}><Icon src={Bars3} class="size-6 text-white" /></button
+		>
+	{/if}
 </nav>
