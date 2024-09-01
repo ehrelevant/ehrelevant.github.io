89be00e599611f92b122a0b15b5605bf2f23d634
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-01 18:56:00 +0800

    feat: add scale animation to about section and prefer left-aligned text

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -4,17 +4,17 @@
 
 <section id="about" class="px-4 sm:px-20 lg:px-32 py-20">
 	<div
-		class="p-4 sm:p-12 border shadow-lg rounded-xl flex flex-col xl:flex-row justify-center gap-4 xl:gap-8"
+		class="p-4 sm:p-12 border shadow-lg rounded-xl flex flex-col xl:flex-row justify-center gap-4 xl:gap-8 hover:scale-105 transition-all"
 	>
 		<h2 class="font-bold text-4xl xl:w-32 text-center xl:text-end">ABOUT ME</h2>
 		<div class="flex flex-col gap-4">
-			<p class="text-lg sm:text-xl text-justify">
+			<p class="text-lg sm:text-xl">
 				I'm a Junior student currently pursuing a Bachelor's degree in Computer Science at the
 				University of the Philippines Diliman. Here, I have been honing my skills in programming,
 				problem solving, teamwork, and leadership to expand my capabilities as a web & software
 				developer.
 			</p>
-			<p class="text-lg sm:text-xl text-justify">
+			<p class="text-lg sm:text-xl">
 				If you are looking to recruit/hire somebody who is hard working, passionate, and willing to
 				learn, then feel free to contact me at
 				<Link href="mailto:ehrencastillo@gmail.com">ehrencastillo@gmail.com</Link>.
