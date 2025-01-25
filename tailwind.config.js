dc6e661fcfe013847b661df384413a148fa97752
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-25 17:23:06 +0000

    feat(tailwind.config.js): add custom colors and box-shadow styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
 		extend: {
 			colors: {
 				'dark-gray': '#292f36',
-				'pink-red': '#fb7185'
+				'pink-red': '#FB7185',
+				primary: '#FB7185',
+				'primary-hover': '#FD8295',
+				black: '#080a0f',
+				dark: {
+					bg: '#1e222a',
+					bg2: '#32353e',
+					hover: '#434d60',
+					text: '#F8F8F8'
+				},
+				light: {
+					bg: '#F8F8F8',
+					bg2: '#D8D8D8',
+					hover: '#B6B6B6',
+					text: '#242424'
+				}
+			},
+			boxShadow: {
+				solid: '5px 5px 0 0 #000000',
+				'solid-lg': '10px 10px 0 0 #000000'
 			},
 			fontFamily: {
 				inter: ['Inter', 'sans-serif', 'system-ui']
