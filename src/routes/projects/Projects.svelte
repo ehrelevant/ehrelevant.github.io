757cc4fac28610977d0365b040f8694304bd255e
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-27 00:45:27 +0800

    feat(projects): add sim spam slam project

--- src/routes/projects/Projects.svelte
+++ src/routes/projects/Projects.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Link from '$lib/components/Link.svelte';
 	import portfolioThumb from '$lib/images/portfolio/thumb.png';
+	import spamThumb from '$lib/images/simSpamSlam/thumb.png';
 
 	import ProjectCard from './ProjectCard.svelte';
 </script>
@@ -21,10 +22,25 @@
 			github="https://github.com/ehrelevant/ehrelevant.github.io"
 		>
 			{#snippet description()}
-				This portfolio website is one of my most recent projects. It was made using
-				<Link href="https://kit.svelte.dev/" toBlank={true}>SvelteKit</Link> and
-				<Link href="https://tailwindcss.com/" toBlank={true}>Tailwind CSS</Link>. I made this
-				website to provide a quick overview of me, my skills, and my past projects.
+				This portfolio website was made using
+				<Link href="https://kit.svelte.dev/" toBlank>SvelteKit</Link> and
+				<Link href="https://tailwindcss.com/" toBlank>Tailwind CSS</Link>. I made this website to
+				provide a quick overview of me, my skills, and my past projects.
+			{/snippet}
+		</ProjectCard>
+		<ProjectCard
+			name="SIM Spam Slam"
+			thumbnail={spamThumb}
+			link="https://ktrin-u.github.io/CS132-Arbitrary/"
+			github="https://github.com/ktrin-u/CS132-Arbitrary"
+		>
+			{#snippet description()}
+				For our CS 132 project, we explored spam SMS data using NLP and clustering techniques to
+				determine the existence of any patterns within them and their contents differed before and
+				after the <Link
+					href="https://lawphil.net/statutes/repacts/ra2022/ra_11934_2022.html"
+					toBlank>SIM Card Registration Act</Link
+				> was imposed.
 			{/snippet}
 		</ProjectCard>
 	</div>
