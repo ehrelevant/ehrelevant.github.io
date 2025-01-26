33261a28bf66913953bee59c2df6415ecbd64b14
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-26 22:52:48 +0800

    feat(nav): add mobile navbar

--- src/routes/MobileNavBtn.svelte
+++ src/routes/MobileNavBtn.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import type { Snippet } from 'svelte';
+
 	interface Props {
 		href: string;
-		isCurrent?: boolean;
-		children?: import('svelte').Snippet;
+		children: Snippet;
 	}
 
-	let { href, isCurrent = false, children }: Props = $props();
+	let { href, children }: Props = $props();
 
-	let colors = $derived(isCurrent ? 'bg-dark-gray text-white' : 'hover:bg-slate-100');
+	let isCurrent = $derived(page.url.pathname === href);
+	let colors = $derived(
+		isCurrent
+			? 'bg-black text-primary hover:shadow-primary'
+			: 'bg-dark-bg2 hover:bg-dark-hover hover:shadow-black'
+	);
 </script>
 
 <a
 	{href}
-	class="block w-full py-4 text-center rounded-xl text-2xl font-semibold hover:scale-105 transition-all {colors}"
+	class="block w-full py-4 text-center hover:shadow-solid border-4 border-black text-2xl font-semibold hover:scale-105 transition-all {colors}"
 	>{@render children?.()}</a
 >
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition';
 	import NavBtn from './NavBtn.svelte';
+	import { quadInOut } from 'svelte/easing';
+	import { Bars3, XMark } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import MobileNavBtn from './MobileNavBtn.svelte';
+
+	let isMenuOpen = $state(false);
+
+	function openMenu() {
+		isMenuOpen = true;
+	}
+
+	function closeMenu() {
+		isMenuOpen = false;
+	}
 </script>
 
 <nav
-	class="bg-dark-bg2 text-dark-text sticky top-0 z-50 w-full shadow-lg h-16 border-b-4 box-content border-black"
+	class="hidden sm:block bg-dark-bg2 text-dark-text sticky top-0 z-50 w-full shadow-lg h-16 border-b-4 box-content border-black"
 >
 	<ul class="h-full flex justify-center items-center gap-3">
 		<li>
@@ -16,4 +31,36 @@
 			<NavBtn href="/projects">Projects</NavBtn>
 		</li>
 	</ul>
+</nav>
+
+<nav class="block sm:hidden">
+	{#if isMenuOpen}
+		<div
+			class="z-40 fixed w-full h-screen bg-dark-bg2/90 backdrop-blur-md flex justify-center items-start"
+			transition:fly={{ duration: 100, x: 500, easing: quadInOut }}
+		>
+			<button
+				class="z-50 fixed bg-black group p-3 top-4 right-4 border-2 border-black hover:scale-105 hover:shadow-solid hover:shadow-primary transition-all"
+				onclick={closeMenu}
+				><Icon src={XMark} class="size-6 group-hover:text-primary transition-colors" /></button
+			>
+			<ul class="flex flex-col w-full justify-center gap-3 m-6 mt-20">
+				<li transition:fly={{ delay: 25, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="/">Ehren</MobileNavBtn>
+				</li>
+				<li transition:fly={{ delay: 75, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="/about">About</MobileNavBtn>
+				</li>
+				<li transition:fly={{ delay: 125, duration: 100, x: 500, easing: quadInOut }}>
+					<MobileNavBtn href="/projects">Projects</MobileNavBtn>
+				</li>
+			</ul>
+		</div>
+	{:else}
+		<button
+			class="z-50 fixed bg-dark-bg2 p-3 top-4 right-4 border-2 border-black hover:scale-105 hover:bg-dark-hover hover:shadow-solid hover:shadow-black transition-all"
+			transition:fly={{ duration: 100, x: 500, easing: quadInOut }}
+			onclick={openMenu}><Icon src={Bars3} class="size-6" /></button
+		>
+	{/if}
 </nav>
