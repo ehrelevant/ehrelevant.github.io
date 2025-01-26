814c95d3f23ad4054929e07e79b785704acddb1f
Ehren Castillo <ehrencastillo@gmail.com>
2025-01-27 00:27:26 +0800

    refactor(ToolBadge.svelte): prefer background on container to avoid white outlines

--- src/routes/about/ToolBadge.svelte
+++ src/routes/about/ToolBadge.svelte
@@ -8,6 +8,14 @@
 	let { name, src, href = '' }: Props = $props();
 </script>
 
+{#snippet toolNameTag(name: string)}
+	<div class="bg-black w-full">
+		<p class="text-base font-bold w-full text-center py-1 group-hover:text-primary transition-all">
+			{name}
+		</p>
+	</div>
+{/snippet}
+
 {#if href}
 	<a
 		{href}
@@ -15,21 +23,13 @@
 		class="flex flex-col justify-center bg-dark-bg2 items-center border-4 border-black hover:shadow-solid hover:shadow-primary w-28 group aspect-square hover:scale-105 transition-all"
 	>
 		<img {src} alt={name} class="grow w-12 h-12" />
-		<p
-			class="text-base font-bold bg-black w-full text-center py-1 group-hover:text-primary transition-all"
-		>
-			{name}
-		</p>
+		{@render toolNameTag(name)}
 	</a>
 {:else}
 	<div
 		class="flex flex-col justify-center bg-dark-bg2 items-center border-4 border-black hover:shadow-solid hover:shadow-primary w-28 group aspect-square hover:scale-105 transition-all"
 	>
 		<img {src} alt={name} class="grow w-12 h-12" />
-		<p
-			class="text-base font-bold bg-black w-full text-center py-1 group-hover:text-primary transition-all"
-		>
-			{name}
-		</p>
+		{@render toolNameTag(name)}
 	</div>
 {/if}
